00f018313d50f75f93c2ebcfc35e0ddfd0346ebb
YoussefHarizi <youssef.harizi@gmail.com>
2021-10-23 09:13:49 +0100

    refactor right navbar

--- frontend_notion_app/src/components/layouts/TheNavbar.vue
+++ frontend_notion_app/src/components/layouts/TheNavbar.vue
@@ -1,33 +1,29 @@
 <template>
-  <div class="flex items-center justify-between h-20">
+  <div class="flex items-center justify-between h-20 text-sm font-medium">
     <div>
       <logo />
     </div>
 
-    <ul class="flex justify-between divide-x divide-green-500">
-      <li class="mr-2 ">Contact sales</li>
-      <li class="mr-2">Login</li>
-      <li
+    <div class="flex items-center ">
+    <div class="divide-x divide-gray-200">
+        <router-link to='/' class="mr-2 p-2 hover:bg-gray-200 rounded">Contact sales</router-link>
+        <router-link to='/' class="ml-2 p-2 hover:bg-gray-200 rounded">Log in</router-link>
+    </div>
+      <router-link to='/'
         class="
-          text-sm
-          
           bg-blue-500
           text-white
           py-1
           px-4
-          cursor-pointer
+         ml-2
           rounded-lg
           hover:bg-blue-400
           focus:outline-none
-          focus-visible:ring-2
-          focus-visible:ring-blue-700
-          focus-visible:ring-offset-2
-          focus-visible:ring-offset-blue-900
+          
         "
+        >Try notion free</router-link
       >
-        Try notion free
-      </li>
-    </ul>
+    </div>
   </div>
 </template>
 
